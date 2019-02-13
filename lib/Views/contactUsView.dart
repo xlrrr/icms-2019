75aa3c937f7e47ff6c5d673f494a4bd7fa7a9e87
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2019-02-13 23:45:51 +0200

    * Fixed ContactUs view

--- lib/Views/contactUsView.dart
+++ lib/Views/contactUsView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsMenu extends StatelessWidget {
 
@@ -28,8 +29,107 @@ class ContactUsMenu extends StatelessWidget {
               Center(child: Stack(
                 children: <Widget>[
                   new Container(
-                    child: Image.asset("assets/contactshexagon.png"),
-                    padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 45.0),)
+                    decoration: new BoxDecoration(
+                      image: new DecorationImage(
+                        image: new AssetImage("assets/contactshexagon.png"),
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                    child: Center(
+                      child: IntrinsicWidth(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {
+                                  onWebsiteTap();
+                                },
+                                child: Row(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(left: 50),
+                                        height: 42.0,
+                                        width: 80.0,
+                                        child: Image.asset("assets/icons/iconearthgradient.png"),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10, right: 50),
+                                        height: 42.0,
+                                        width: 180.0,
+                                        child: Image.asset("assets/site.png"),
+                                      ),
+                                    ]
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  onFacebookTap();
+                                },
+                                child: Row(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(left: 50),
+                                        height: 42.0,
+                                        width: 80.0,
+                                        child: Image.asset("assets/icons/iconfbgradient.png"),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10, right: 50),
+                                        height: 42.0,
+                                        width: 150.0,
+                                        child: Image.asset("assets/fbtwitter.png"),
+                                      ),
+                                    ]
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  onTwitterTap();
+                                },
+                                child: Row(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(left: 50),
+                                        height: 42.0,
+                                        width: 80.0,
+                                        child: Image.asset("assets/icons/icontwittergradient.png"),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10, right: 50),
+                                        height: 42.0,
+                                        width: 150.0,
+                                        child: Image.asset("assets/fbtwitter.png"),
+                                      ),
+                                    ]
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  onEmailTap();
+                                },
+                                child: Row(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(left: 50),
+                                        height: 42.0,
+                                        width: 80.0,
+                                        child: Image.asset("assets/icons/iconmailgradient.png"),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10, right: 50),
+                                        height: 42.0,
+                                        width: 180.0,
+                                        child: Image.asset("assets/mailcontacts icon.png"),
+                                      ),
+                                    ]
+                                ),
+                              )
+                            ],
+                          )
+                      )
+                    ),
+                    margin: EdgeInsets.all(50),
+                  )
                 ],
               )
               ),
@@ -72,5 +172,41 @@ class ContactUsMenu extends StatelessWidget {
           ),
         )
     );
+  }
+
+  onWebsiteTap() async {
+    const url = 'https://icmsbg.org';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  onFacebookTap() async {
+    const url = 'fb://facewebmodal/ICMSBulgaria/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  onTwitterTap() async {
+    const url = 'https://twitter.com/icmsbulgaria';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  onEmailTap() async {
+    const url = 'mailto:info@icmsbg.org';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
